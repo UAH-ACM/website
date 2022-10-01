488802ac306a4340abc2d6d7c7eaccb6904eb850
shorewind <99942150+shorewind@users.noreply.github.com>
2022-10-01 00:33:14 -0500

    added assets and set functional buttons

--- members/esthershore/script.js
+++ members/esthershore/script.js
@@ -1,4 +1,13 @@
-function show(){
-    document.getElementById("button").style.display = "none";
-    document.getElementById("button-container").textContent = "Button clicked";
+function redirect(url) {
+    window.location.href = url;
+}
+
+function refresh() {
+    window.location.reload();
+}
+
+function pop() {
+    let bubble = document.getElementById("bubble-button");
+    bubble.style.color = "#E7F6F2";
+    bubble.style.background = "#2C3333"
 }
